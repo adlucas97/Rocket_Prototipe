a06c90894bf07889a505080bd4bece0c6c257bb9
Jefrey Hipp <47755694+jefreyhipp@users.noreply.github.com>
2022-06-28 17:42:57 -0600

    Update MPU6050_Data_Read.ino

--- MPU6050_Data_Read.ino
+++ MPU6050_Data_Read.ino
@@ -1,49 +1,87 @@
-#include <VirtualWire.h>
-#include <VirtualWire_Config.h>
-#include <MPU6050.h>
-#include <I2Cdev.h>
-
-MPU6050 sensor;
-
-// Valores RAW (sin procesar) del acelerometro y giroscopio en los ejes x,y,z
-int ax, ay, az;
-int gx, gy, gz;
-String a_xyz = "";
-String filtro_complemento = ""; 
-
+#include "Wire.h"
+ 
+//ADDR MPU6050 
+#define MPU_ADDR 0x68
+ 
+//Conversion Ratio
+#define Acc_R 16384.0   // 32768/2
+#define Gyro_R 131.0     // 32768/250
+#define E_gravity 9.81  //Earth gravity
+ 
+//Rad to Grad
+#define RAD_TO_DEG 57.295779
+ 
+//RAW values
+int16_t acc_x, acc_y, acc_z;
+int16_t gyro_x, gyro_y, gyro_z;
+int16_t temperature; 
+ 
+//variables useful to determinde Yaw, Pitch, Roll
+float acc[3];
+float ang_acc[3]; 
+float gyro[3];
+float angle[3];
+ 
 long tiempo_prev;
 float dt;
-float ang_x, ang_y;
-float ang_x_prev, ang_y_prev;
 
-void setup() {
-  Serial.begin(57600);    //Iniciando puerto serial
-  Wire.begin();           //Iniciando I2C  
-  sensor.initialize();    //Iniciando el sensor
+String valores;
+ 
+void setup()
+{
+ Wire.begin(); // D2(GPIO4)=SDA / D1(GPIO5)=SCL
+ Wire.beginTransmission(MPU_ADDR);
+ Wire.write(0x6B);
+ Wire.write(0);
+ Wire.endTransmission(true);
+ Serial.begin(115200);
 }
+ 
+void loop()
+{
+   
+   Wire.beginTransmission(MPU_ADDR);
+   Wire.write(0x3B); //starting with register 0x3B (ACCEL_XOUT_H)
+   Wire.endTransmission(false); //The parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
+   Wire.requestFrom(MPU_ADDR, 7*2, true); // request a total of 7*2=14 registers
 
-void loop() {
-  // Leer las aceleraciones y velocidades angulares
-  sensor.getAcceleration(&ax, &ay, &az);
-  sensor.getRotation(&gx, &gy, &gz);
+   acc_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
+   acc_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
+   acc_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
+   
+   temperature = Wire.read()<<8 | Wire.read(); // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
+   
+   gyro_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
+   gyro_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
+   gyro_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L) 
 
-  float ax_m_s2 = ax * (9.81/16384.0);
-  float ay_m_s2 = ay * (9.81/16384.0);
-  float az_m_s2 = az * (9.81/16384.0);
-  float gx_deg_s = gx * (250.0/32768.0);
-  float gy_deg_s = gy * (250.0/32768.0);
-  float gz_deg_s = gz * (250.0/32768.0);
+   //Acceleration Calculation
+   acc[0] = (acc_x/Acc_R)*E_gravity;
+   acc[1] = (acc_y/Acc_R)*E_gravity;
+   acc[2] = (acc_z/Acc_R)*E_gravity;
+   
+   //Roll and Pitch calculation
+   ang_acc[1] = atan(-1*(acc[0])/sqrt(pow((acc[1]),2) + pow((acc[3]),2)))*RAD_TO_DEG;
+   ang_acc[0] = atan((acc[1])/sqrt(pow((acc[0]),2) + pow((acc[3]),2)))*RAD_TO_DEG;
 
-  a_xyz = String(ax_m_s2)+","+String(ay_m_s2)+","+String(az_m_s2);
-
-  //Mostrar las lecturas separadas por un [tab]
-  //Serial.print("a[x y z] g[x y z]:\t");
-  Serial.println(a_xyz);
-  //Serial.print(ay); Serial.print("\t");
-  //Serial.print(az); Serial.print("\t");
-  //Serial.print(gx); Serial.print("\t");
-  //Serial.print(gy); Serial.print("\t");
-  //Serial.println(gz);
-
-  delay(100);
+   //Angular Rate Calculation
+   gyro[0] = gyro_x/Gyro_R;
+   gyro[1] = gyro_y/Gyro_R;
+   gyro[2] = gyro_z/Gyro_R;
+ 
+   dt = (millis() - tiempo_prev) / 1000.0;
+   tiempo_prev = millis();
+ 
+   //Aplicar el Filtro Complementario
+   angle[0] = 0.98 *(angle[0]+gyro[0]*dt) + 0.02*gyro[0];
+   angle[1] = 0.98 *(angle[1]+gyro[1]*dt) + 0.02*gyro[1];
+ 
+   //Integración respecto del tiempo paras calcular el YAW
+   angle[2] = angle[2]+gyro[2]*dt;
+ 
+   //Mostrar los valores por consola
+   valores = "90, " +String(angle[0]) + "," + String(angle[1]) + "," + String(angle[2]) + ", -90";
+   Serial.println(valores);
+   
+   delay(10);
 }
